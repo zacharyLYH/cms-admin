9fc261cbb7d444ae920a648f75e5e23dea69d9bb
Zachary <leeyihong03@gmail.com>
2023-07-02 10:51:46 -0400

    refactored layout code to page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,28 @@
-const DashboardPage = () => {
-    return (
-        <div>
-            This is a dashboard
-        </div>
-    )
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface DashboardProps {
+    params: {
+        storeId: string;
+    };
 }
 
-export default DashboardPage
+const DashboardPage: React.FC<DashboardProps> = async ({ params }) => {
+    const { userId } = auth();
+    if (!userId) {
+        redirect("/sign-in");
+    }
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId: userId,
+        },
+    });
+    if (!store) {
+        redirect("/");
+    }
+    return <div>This is a dashboard</div>;
+};
+
+export default DashboardPage;
