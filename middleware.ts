98df8f65a01ce83068e22d8da4d36053d9c642b2
Zachary <leeyihong03@gmail.com>
2023-07-03 14:14:02 -0400

    Data table done

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+    publicRoutes: ["/api/:path*"], //exposes all the APIs to public. BUT, the private routes have their own authentication inside already using auth(), so this is valid.
+});
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
